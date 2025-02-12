e9e0b142c89186b010a1f862374b0ec22595d18e
Aiert Basante <otxoboy64@gmail.com>
2025-02-12 13:01:01 +0100

    finished recursive logic of class page

--- 02-Oob/ex06/Page.py
+++ 02-Oob/ex06/Page.py
@@ -21,6 +21,49 @@ class Page(Elem):
             return False
         if type(elem_instance) == Text or isinstance(elem_instance, Meta):
             return True
+        if isinstance(elem_instance, Html):
+            content = elem_instance.content
+            if len(content) == 2:
+                if type(content[0], Head) and type(children[1], Body):
+                    if (all(self._check(el) for el in content)):
+                        return True
+        elif isinstance(elem_instance, Head):
+            content = elem_instance.content
+            if [isinstance(el, Title) for el in content].count(True) == 1:
+                if (all(self._check(el) for el in content)):
+                    return True
+        elif isinstance(elem_instance, (Body, Div)):
+            allowed_types = (H1, H2, Div, Table, Ul, Ol, Span, Text)
+            content = elem_instance.content
+            if all(isinstance(el, allowed_types) for el in content):
+                if all(self._check(el) for el in content):
+                    return True
+        elif isinstance(elem_instance, (Title, H1, H2, Li, Th, Td)):
+            content = elem_instance.content
+            if len(content) == 1 and type(content[0]) == Text:
+                return True
+        elif isinstance(elem_instance, P):
+            content = elem_instance.content
+            if all([isinstance(el, Text) for el in content]):
+                return True
+        elif isinstance(elem_instance, Span):
+            if all([isinstance(el, (Text, P) for el in elem_instance.content)]):
+                if (all(self._check(el) for el in elem_instance.content)):
+                    return True
+        elif isinstance(elem_instance, (Ol, Ul)) and len(elem.content) > 0 and \
+            all([isinstance(el, Li) for el in elem_instance.content]):
+                if (all(self._check(el) for el in elem_instance.content)):
+                    return True
+        elif isinstance(elem_instance, Tr) and len(elem_instance.content) > 0 and \
+            all([isinstance(el, (Th, Td)) for el in elem_instance.content]) and \
+                all([type(el) == type(elem_instance.content[0]) for el in elem_instance.content]):
+                    return True
+        elif isinstance(elem_instance, Table) and \
+            all([isinstance(el, Tr) for el in elem_instance.content]):
+                return True
+        return False
+
+                
 
 def main():
     page1 = Page(Some())
