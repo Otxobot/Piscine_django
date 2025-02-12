c2b55148425fa3f974540d200e150a9e889c1ab5
Aiert Basante <otxoboy64@gmail.com>
2025-02-12 14:53:54 +0100

    ex01 in progress

---
+++ 10-Lib/ex01/my_program.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+sys.path.insert(0, "./local_lib")
+
+
+from path import Path
+
---
+++ 10-Lib/ex01/my_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pip --version
+
+INSTALL_DIR="./local_lib"
+LOG_FILE="install_path.log"
+
+
+if [ -d "$INSTALL_DIR" ]; then
+  echo "Removing existing local_lib folder..." | tee -a $LOG_FILE
+  rm -rf "$INSTALL_DIR"
+fi
+
+echo "Installing path.py development version..." | tee -a $LOG_FILE
+
+pip install git+https://github.com/jaraco/path.git --target="$INSTALL_DIR" >> $LOG_FILE 2>&1
+
+if [ $? -eq 0 ]; then
+  echo "path.py installed successfully." | tee -a $LOG_FILE
+
+  python3 my_program.py
+else
+  echo "Installation failed. Check the log file for errors." | tee -a $LOG_FILE
+fi
